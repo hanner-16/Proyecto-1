2d8d5fe7370c1c1edf12c6196d56dd520890c5c4
Hanner <hanner.sinisterra@correounivalle.edu.co>
2023-04-20 15:30:30 -0500

    Condicionales de letras

--- src/game/JuegoLetras.java
+++ src/game/JuegoLetras.java
@@ -67,4 +67,53 @@ private String seleccionado;
                 null,
                 themes,
                 themes[0]);    
+    switch (opcion){
+            
+            case 0:
+                    seleccionado = ANIMALES[random.nextInt(ANIMALES.length)];    
+
+                
+                break;
+                
+            
+            case 1:
+                
+                     seleccionado = FRUTAS[random.nextInt(FRUTAS.length)];
+                
+                break;
+                
+                
+            case 2:
+                
+                     seleccionado = COLORES[random.nextInt(COLORES.length)];
+                
+                break;   
+            
+            case 3:
+                
+                    seleccionado = INSTRUMENTOS[random.nextInt(INSTRUMENTOS.length)];
+                
+                break;
+            
+             
+            default:
+                return new String[]{};
+           
+        }
+    
+    
+    int vocalIndex = -1;
+    while (vocalIndex == -1) { // buscar una vocal aleatoria que no haya sido eliminada
+        vocalIndex = random.nextInt(seleccionado.length()); // seleccionar índice de vocal aleatorio
+        char vocal = seleccionado.charAt(vocalIndex);
+        if (esVocal(vocal)) { // si la letra seleccionada es una vocal, eliminarla
+            seleccionado = seleccionado.substring(0, vocalIndex)+ "_"  + seleccionado.substring(vocalIndex + 1);
+            vocalIncompleta = vocal; // guardar vocal eliminada
+        } else {
+            vocalIndex = -1; // si no es vocal, seguir buscando
+        }
     }
+        return null;
+        
+       
+}
